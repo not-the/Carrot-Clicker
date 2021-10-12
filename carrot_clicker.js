9b2f7367368306b62b148e1544d4f64b0d9644f5
J.J <88807892+JJCVIP@users.noreply.github.com>
2021-10-12 17:10:11 -0400

    Keybinds
    Space onClick
    1 up bill
    2 up belle
    3 up Gregory
    4 wooden hoe
    5 stone hoe
    6 iron hoe
    7 gold hoe
    8 diamond hoe
    9 netherite hoe
    num+option equip to bill
    num+ctrl equip to Belle
    p Prestige
    ctrl+delete clear local storage

--- carrot_clicker.js
+++ carrot_clicker.js
@@ -99,13 +99,130 @@ const elCharles = {
     charlesTooltip:           dom("charlestooltip")
 }
 
-//Keyboard Event listener
-window.addEventListener('keydown', (event)=>{
+
+//#endregion
+
+
+/*-----------------------Keybinds-------------------*/
+//#region
+
+
+//defining keybinds
+function keybinds(event){
     if(event.key == "Shift"){
         multibuySpin();
         dom("multibuy").innerText=(multibuy[multibuySelector]+"x");
     }
-})
+    if(event.key == " "){
+        event.preventDefault();
+        onClick();
+    }
+
+    //level up
+    if(event.key=="1" && event.altKey==false && event.ctrlKey==false){
+        LevelUp(Boomer_Bill,multibuy[multibuySelector]);
+    }
+    if(event.key=="2" && event.altKey==false && event.ctrlKey==false){
+        LevelUp(Belle_Boomerette,multibuy[multibuySelector]);
+    }
+    if(event.key=="3" && event.altKey==false && event.ctrlKey==false){
+        LevelUp(Gregory,multibuy[multibuySelector]);
+    }
+
+    //hoes
+    if(event.key=="4" && event.altKey==false && event.ctrlKey==false){
+        CreateHoe(0,multibuy[multibuySelector]);
+    }
+    if(event.key=="5" && event.altKey==false && event.ctrlKey==false){
+        CreateHoe(1,multibuy[multibuySelector]);
+    }
+    if(event.key=="6" && event.altKey==false && event.ctrlKey==false){
+        CreateHoe(2,multibuy[multibuySelector]);
+    }
+    if(event.key=="7" && event.altKey==false && event.ctrlKey==false){
+        CreateHoe(3,multibuy[multibuySelector]);
+    }
+    if(event.key=="8" && event.altKey==false && event.ctrlKey==false){
+        CreateHoe(4,multibuy[multibuySelector]);
+    }
+    if(event.key=="9" && event.altKey==false && event.ctrlKey==false){
+        CreateHoe(5,multibuy[multibuySelector]);
+    }
+
+
+    if(event.key=="4" && event.altKey==true && event.ctrlKey==false){
+        EquipHoe(Boomer_Bill,0,multibuy[multibuySelector]);
+    }
+    if(event.key=="5" && event.altKey==true && event.ctrlKey==false){
+        EquipHoe(Boomer_Bill,1,multibuy[multibuySelector]);
+    }
+    if(event.key=="6" && event.altKey==true && event.ctrlKey==false){
+        EquipHoe(Boomer_Bill,2,multibuy[multibuySelector]);
+    }
+    if(event.key=="7" && event.altKey==true && event.ctrlKey==false){
+        EquipHoe(Boomer_Bill,3,multibuy[multibuySelector]);
+    }
+    if(event.key=="8" && event.altKey==true && event.ctrlKey==false){
+        EquipHoe(Boomer_Bill,4,multibuy[multibuySelector]);
+    }
+    if(event.key=="9" && event.altKey==true && event.ctrlKey==false){
+        EquipHoe(Boomer_Bill,5,multibuy[multibuySelector]);
+    }
+
+    if(event.key=="4" && event.altKey==false && event.ctrlKey==true){
+        event.preventDefault();
+        EquipHoe(Belle_Boomerette,0,multibuy[multibuySelector]);
+    }
+    if(event.key=="5" && event.altKey==false && event.ctrlKey==true){
+        event.preventDefault();
+        EquipHoe(Belle_Boomerette,1,multibuy[multibuySelector]);
+    }
+    if(event.key=="6" && event.altKey==false && event.ctrlKey==true){
+        event.preventDefault();
+        EquipHoe(Belle_Boomerette,2,multibuy[multibuySelector]);
+    }
+    if(event.key=="7" && event.altKey==false && event.ctrlKey==true){
+        event.preventDefault();
+        EquipHoe(Belle_Boomerette,3,multibuy[multibuySelector]);
+    }
+    if(event.key=="8" && event.altKey==false && event.ctrlKey==true){
+        event.preventDefault();
+        EquipHoe(Belle_Boomerette,4,multibuy[multibuySelector]);
+    }
+    if(event.key=="9" && event.altKey==false && event.ctrlKey==true){
+        event.preventDefault();
+        EquipHoe(Belle_Boomerette,5,multibuy[multibuySelector]);
+    }
+    
+    //settings and prestige
+    if(event.key=="Backspace" && event.altKey==false && event.ctrlKey==true){
+        event.preventDefault();
+        openDialog('Are you sure?', 'Your progress will be lost forever!', 'Delete Save Data', 'button_red', 'clearsave');
+    }
+    if(event.key=="p"){
+        openDialog('Are you Sure you want to Prestige?', 'Your carrots, characters, and upgrades will be lost, but you will gain a permanent earnings boost.', 'Prestige', 'button_gold', 'prestige');
+    }
+} 
+var checkBox = dom("disable_keybinds");
+var keybindsLS = localStorage.getItem("keybinds")
+if(keybindsLS=="false"){
+    checkBox.checked=true;
+}
+//Keyboard Event listener and checkboxing
+function KeyBinds() {
+    //checkbox
+  
+    //checking checkbox
+    if (checkBox.checked == true){
+        toast("Settings","Keybinds are Disabled","red");
+        window.removeEventListener('keydown',keybinds);
+        localStorage.setItem("keybinds","false");
+        return;
+    }
+        window.addEventListener('keydown',keybinds);
+        localStorage.setItem("keybinds","true");
+}
+
 //#endregion
 
 
@@ -181,6 +298,26 @@ const Boomer_Bill      = localStorage.getObject("Bill");
 const Belle_Boomerette = localStorage.getObject("Belle");
 const Gregory          = localStorage.getObject("Greg");
 
+//autosaves
+setInterval(() => {
+    if(player){
+        localStorage.setObject("player",player);
+        localStorage.setObject("Bill",Boomer_Bill);
+        localStorage.setObject("Belle",Belle_Boomerette);
+        localStorage.setObject("Greg",Gregory);
+        localStorage.setObject("Charles",Charles)
+    }else{
+        localStorage.setObject("player",player1);
+        localStorage.setObject("Bill",Default_Boomer_Bill);
+        localStorage.setObject("Belle",Default_Belle_Boomerette);
+        localStorage.setObject("Greg",Default_Gregory);
+        localStorage.setObject("Charles",Default_Charles);
+        location.reload();
+    }
+}, 2000);
+
+
+KeyBinds();
 //#endregion
 
 
@@ -329,11 +466,6 @@ function IncreaseDecreaseWages(){
 function DecreaseWagesEffects(){
     return (Math.sqrt(Charles.DecreaseWages)/100);
 }
-
-
-
-
-
 
 //#endregion
 
@@ -677,29 +809,6 @@ setInterval(() => {
        BH: ${Math.floor(100*Charles.BetterHoes)}%\n
        DWW: ${Math.floor(100*Charles.DecreaseWages)}%`;
 }, 100);
-//#endregion
-
-
-/*---------Auto Saves----------*/
-//#region
-
-setInterval(() => {
-    if(player){
-        localStorage.setObject("player",player);
-        localStorage.setObject("Bill",Boomer_Bill);
-        localStorage.setObject("Belle",Belle_Boomerette);
-        localStorage.setObject("Greg",Gregory);
-        localStorage.setObject("Charles",Charles)
-    }else{
-        localStorage.setObject("player",player1);
-        localStorage.setObject("Bill",Default_Boomer_Bill);
-        localStorage.setObject("Belle",Default_Belle_Boomerette);
-        localStorage.setObject("Greg",Default_Gregory);
-        localStorage.setObject("Charles",Default_Charles);
-        location.reload();
-    }
-}, 2000);
-
 //#endregion
 
 
