8eee8113f6c6a6a74fe858cb1ae4d3c4c512f14c
not-the <87151784+not-the@users.noreply.github.com>
2022-09-07 20:57:02 -0400

    Dev beta 1.16.10

--- gamedata.js
+++ gamedata.js
@@ -1,7 +1,7 @@
 // Game data
 // Game version
 (() => {
-    const game_version = 'dev beta v1.16.9.1';
+    const game_version = 'dev beta v1.16.10';
     dom('page_title').innerText = `Carrot Clicker ${game_version}`;
     dom('footer_version').innerText = `Version ${game_version} - Unstable`;
 })()
